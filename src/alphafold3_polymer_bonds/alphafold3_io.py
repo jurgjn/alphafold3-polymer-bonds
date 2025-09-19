2961123807d9ed6303d7835709ec063af9d92074
jurgjn <jurgjn@users.noreply.github.com>
2025-09-19 16:38:10 +0200

    Missing import, dependancy

--- src/alphafold3_polymer_bonds/alphafold3_io.py
+++ src/alphafold3_polymer_bonds/alphafold3_io.py
@@ -1,5 +1,5 @@
 
-import argparse, collections, copy, gzip, importlib, importlib.metadata, importlib.resources, json, os, os.path, string, sys
+import argparse, collections, copy, gzip, importlib, importlib.metadata, importlib.resources, json, os, os.path, re, string, sys
 from copy import deepcopy
 from pathlib import Path
 from pprint import pprint
